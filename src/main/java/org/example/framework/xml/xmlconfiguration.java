2e785b4297f5e04f917eeadcded4f097fcfae8c6
nouhaila <nouhaila@exemple.com>
2025-03-02 16:05:45 +0000

    Ajout du XmlConfiguration ou il ya les dependances

--- src/main/java/org/example/framework/xml/xmlconfiguration.java
+++ src/main/java/org/example/framework/xml/xmlconfiguration.java
@@ -1,4 +1,20 @@
 package org.example.framework.xml;
 
-public class xmlconfiguration {
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "configuration")
+public class XmlConfiguration {
+
+    private List<XmlDependency> dependencies;
+
+    @XmlElement(name = "dependency")
+    public List<XmlDependency> getDependencies() {
+        return dependencies;
+    }
+
+    public void setDependencies(List<XmlDependency> dependencies) {
+        this.dependencies = dependencies;
+    }
 }
